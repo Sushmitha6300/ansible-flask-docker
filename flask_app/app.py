8cf3eda41319440961dc6ddaf0f875890424d28e
Sushmitha6300 <sushmithaande004@gmail.com>
2025-07-17 21:19:04 +0530

    Added Flask CI/CD project using Ansible, Jenkins, Docker, and Terraform

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, this is Shravani’s custom Flask app running in Docker via Ansible!"
+    return "Hello, this is Sushmitha’s custom Flask app running in Docker via Ansible!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
