c1d91b60ba7ebe881f1783038cf8cc92102ea550
Sushmitha6300 <sushmithaande004@gmail.com>
2025-07-17 20:25:47 +0530

    Added Flask CI/CD project using Ansible, Jenkins, Docker, and Terraform

--- get-controller-ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to the terraform directory
-cd terraform
-
-# Save controller IP from terraform output to a file in the root directory
-terraform output -raw controller_public_ip > ../controller-ip.txt
